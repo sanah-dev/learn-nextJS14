cfc29dd121fea435bb017c296dca90ef618ad139
박산아 <dev.sanah@gmail.com>
2024-07-27 18:25:27 +0900

    #2.8 Metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Not found',
+};
+
 export default function NotFound() {
-  return <h1>Notfound!</h1>;
+  return (
+    <div>
+      <h1>Not found!</h1>
+    </div>
+  );
 }
