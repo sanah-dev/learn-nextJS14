2ae7163b7d75737173d84f81caeb11e0278c9bb1
박산아 <dev.sanah@gmail.com>
2024-07-28 16:51:09 +0900

    #3.1 Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,34 @@
-export const metadata = {
-  title: 'Home',
-};
+'use client';
 
-export default function Tomato() {
+import { useEffect, useState } from 'react';
+
+// export const metadata = {
+//   title: 'Home',
+// };
+
+export default function Page() {
+  // data fetch
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // is loading...
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
-      <h1>Hello!</h1>
+      <h1>Home</h1>
+      {isLoading ? 'Loading...' : JSON.stringify(movies)}
     </>
   );
 }
